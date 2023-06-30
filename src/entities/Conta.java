1821de90dc1d44b08cdb246d4377d4ef39160730
Leonidas Marques <leonidasmmarques@outlook.com>
2023-06-30 18:30:08 -0300

    iniciando os construtores e variaves de classe

--- src/entities/Conta.java
+++ src/entities/Conta.java
@@ -2,4 +2,27 @@ package entities;
 
 public class Conta {
 
+	private String nome;
+	private int numeroConta;
+	private double depositoInicial;
+	private double saldo;
+	
+	
+	public Conta(String nome, int numeroConta, double saldo) {
+		this.nome = nome;
+		this.numeroConta = numeroConta;
+		this.saldo = saldo;
+	}
+
+
+	public Conta(String nome, int numeroConta, double depositoInicial, double saldo) {
+		this.nome = nome;
+		this.numeroConta = numeroConta;
+		this.depositoInicial = depositoInicial;
+		this.saldo = saldo;
+	}
+	
+	
+	
+	
 }
